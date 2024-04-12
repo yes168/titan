a23348b99f6f045100f19cee6d11edf32c602a07
yes168 <77735809+yes168@users.noreply.github.com>
2024-04-12 19:04:39 +0800

    Update titan.sh

--- titan.sh
+++ titan.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-# 检查是否以root用户运行脚本
-if [ "$(id -u)" != "0" ]; then
-    echo "此脚本需要以root用户权限运行。"
-    echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
-    exit 1
-fi
+# 固定身份码
+id="E221E36F-ACC0-4611-B947-99C1FC512A8B"
 
-echo "脚本以及教程由推特用户大赌哥 @y95277777 编写，免费开源，请勿相信收费"
-echo "================================================================"
-echo "节点社区 Telegram 群组:https://t.me/niuwuriji"
-echo "节点社区 Telegram 频道:https://t.me/niuwuriji"
+# 固定节点数量
+container_count=5
 
-# 读取加载身份码信息
-#read -p "输入你的身份码: " id
+# 固定存储空间大小
+storage_gb=1
 
-# 让用户输入想要创建的容器数量
-#read -p "请输入你想要创建的节点数量，单IP限制最多5个节点: " container_count
-
-# 让用户输入想要分配的空间大小
-#read -p "请输入你想要分配每个节点的存储空间大小（GB），单个上限64G, 网页生效较慢，等待20分钟后，网页查询即可: " storage_gb
-
-# 让用户输入存储路径（可选）
-#read -p "请输入节点存储数据的宿主机路径（直接回车将使用默认路径 titan_storage_$i,依次数字顺延）: " custom_storage_path
+# 固定节点存储数据的宿主机路径
+custom_storage_path="titan_storage"
 
 apt update
 
@@ -41,12 +29,7 @@ fi
 # 拉取Docker镜像
 docker pull nezha123/titan-edge:1.4
 
-# 创建用户指定数量的容器
-id="E221E36F-ACC0-4611-B947-99C1FC512A8B"
-container_count=5
-storage_gb=1
-custom_storage_path="titan_storage_"
-
+# 创建固定数量的容器
 for i in $(seq 1 $container_count)
 do
     # 判断用户是否输入了自定义存储路径
@@ -68,16 +51,14 @@ do
 
     sleep 30
 
-        # 修改宿主机上的config.toml文件以设置StorageGB值
-docker exec $container_id bash -c "\
-    sed -i 's/^[[:space:]]*#StorageGB = .*/StorageGB = $storage_gb/' /root/.titanedge/config.toml && \
-    echo '容器 titan'$i' 的存储空间已设置为 $storage_gb GB'"
+    # 修改宿主机上的config.toml文件以设置StorageGB值
+    docker exec $container_id bash -c "\
+        sed -i 's/^[[:space:]]*#StorageGB = .*/StorageGB = $storage_gb/' /root/.titanedge/config.toml && \
+        echo '容器 titan'$i' 的存储空间已设置为 $storage_gb GB'"
    
     # 进入容器并执行绑定和其他命令
     docker exec $container_id bash -c "\
         titan-edge bind --hash=$id https://api-test1.container1.titannet.io/api/v2/device/binding"
-
-
 done
 
 echo "==============================所有节点均已设置并启动===================================."

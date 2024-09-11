3a3346f3151a51f428bf3ca4c9ff83d2780aec69
yes168 <77735809+yes168@users.noreply.github.com>
2024-09-12 00:17:11 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-command="sudo yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5"
+command="sudo yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5 --fee-rate 1500"
 
 
 while true; do
